798e363b71d57b002f74a6ee949d9341c7c47860
Eduard Slobodyanik <112650697+Altair788@users.noreply.github.com>
2023-05-11 21:09:02 +0300

    2.1 Функция для вычисления индекса массы тела по двум параметрам: рост и вес.

--- 2.1.py
+++ 2.1.py
@@ -1,11 +1,14 @@
-import math
-#  для минимальной нагрузки на память import sqrt from math
-a, b = int(input()), int(input())
-print(a + b)
-print(a - b)
-print(a * b)
-print(a / b)
-print(a // b)
-print(a % b)
-#   print(sqrt(a ** 10 + b ** 10))
-print(math.sqrt(a ** 10 + b ** 10))
+# Функция для вычисления Индекса массы тела
+def body_index(height, weight):
+    height = float(input('Введите Ваш рост (пример: 1.77)   '))
+    weight = float(input('Введите Ваш вес (пример: 60)  '))
+    index_body = weight / height ** 2
+    if 18.5 <= index_body <= 25:
+        return ('Оптимальная масса')
+    elif index_body < 18.5:
+        return ('Недостаточная масса')
+    elif index_body > 25:
+        return ('Избыточная масса')
+
+
+print(body_index(1.77, 70))
