a64a9ccbfc98b2ce4e571408c761f2f41dce76cc
Eduard Slobodyanik <112650697+Altair788@users.noreply.github.com>
2023-05-11 14:02:51 +0300

    На easy
    На вход программе подаются два целых числа
    a
    a и
    b
    b. Напишите программу, которая выводит:
    
    сумму чисел
    a
    a и
    b
    b;
    разность чисел
    a
    a и
    b
    b;
    произведение чисел
    a
    a и
    b
    b;
    частное чисел
    a
    a и
    b
    b;
    целую часть от деления числа
    a
    a на
    b
    b;
    остаток от деления числа
    a
    a на
    b
    b;
    корень квадратный из суммы их
    10
    10-х степеней:
    a
    10
    +
    b
    10
    a
    10
     +b
    10
    
    ​
     .
    Формат входных данных
    На вход программе подаются два целых числа
    a
    a и
    b
    (
    b
    ≠
    0
    )
    b(b
    
    =0), каждое на отдельной строке.
    
    Формат выходных данных
    Программа должна вывести результаты математических операций в соответствии с условием задачи.

---
+++ 2.1.py
@@ -0,0 +1,11 @@
+import math
+#  для минимальной нагрузки на память import sqrt from math
+a, b = int(input()), int(input())
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+#   print(sqrt(a ** 10 + b ** 10))
+print(math.sqrt(a ** 10 + b ** 10))
